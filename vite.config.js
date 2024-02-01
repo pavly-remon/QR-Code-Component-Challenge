c769403443e9642308bb3ee4edf6f428a708efdc
Pavly Remon <pavlyremon@gmail.com>
2024-02-01 13:29:48 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: `/dist/`,
     resolve: {
         alias: [
             // eslint-disable-next-line no-undef
